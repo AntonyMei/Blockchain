403b2718e660afd2e93c385a93f5008282dc2fb4
Antony <meiyixuan2000@126.com>
2022-05-08 21:30:32 +0800

    add utility functions for Tx

--- src/transaction/transaction.go
+++ src/transaction/transaction.go
@@ -1,12 +1,22 @@
 package transaction
 
-import "github.com/AntonyMei/Blockchain/config"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/gob"
+	"github.com/AntonyMei/Blockchain/config"
+	"github.com/AntonyMei/Blockchain/src/utils"
+)
 
 type TxRecord struct {
 	// Value: number of coins used
 	// PubKey: TODO: finish this
 	Value  int
 	PubKey string
+}
+
+func (record *TxRecord) CanUnlock(data string) bool {
+	return record.PubKey == data
 }
 
 type TxSource struct {
@@ -18,10 +28,30 @@ type TxSource struct {
 	Sig        string
 }
 
+func (source *TxSource) CanUnlock(data string) bool {
+	return source.Sig == data
+}
+
 type Transaction struct {
 	TxID         []byte
 	SourceList   []TxSource
 	TxRecordList []TxRecord
+}
+
+func (tx *Transaction) SetID() {
+	// set TxID as hash value of serialized Transaction
+	var encoded bytes.Buffer
+	var hash [32]byte
+	encoder := gob.NewEncoder(&encoded)
+	err := encoder.Encode(tx)
+	utils.Handle(err)
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.TxID = hash[:]
+}
+
+func (tx *Transaction) IsCoinbase() bool {
+	// Check whether a tx is coinbase tx
+	return len(tx.SourceList) == 1 && len(tx.SourceList[0].SourceTxID) == 0 && tx.SourceList[0].RecordIdx == -1
 }
 
 func CoinbaseTx(toAddress string, data string) *Transaction {
