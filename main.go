4a9f6657b2efd279a3e9755fbb238db11a2caf2d
Antony <meiyixuan2000@126.com>
2022-05-08 19:41:28 +0800

    add a test for persistent storage

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/AntonyMei/Blockchain/src/blockchain"
+	"github.com/AntonyMei/Blockchain/src/blocks"
+	"strconv"
 )
 
 func main() {
-	println("Blockchain Chapter 2")
+	println("Persistent Storage Test\n")
 	// create a chain
 	chain := blockchain.InitBlockChain()
 	chain.AddBlock("first block after genesis")
@@ -13,16 +16,17 @@ func main() {
 	chain.AddBlock("third block after genesis")
 
 	// print info
-	//for _, block := range chain.BlockList {
-	//	fmt.Printf("Previous hash: %x\n", block.PrevHash)
-	//	fmt.Printf("hash: %x\n", block.Hash)
-	//	fmt.Printf("data: %s\n", block.Data)
-	//	fmt.Printf("nonce: %v\n", block.Nonce)
-	//	fmt.Printf("nonce: %v\n", block.Difficulty)
-	//
-	//	pow := blocks.CreateProofOfWork(block)
-	//	fmt.Printf("Pow validated: %s\n", strconv.FormatBool(pow.ValidateNonce()))
-	//	fmt.Println()
-	//}
+	for iterator := chain.Iterator(); iterator.Next(); {
+		block := iterator.GetVal()
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("hash: %x\n", block.Hash)
+		fmt.Printf("data: %s\n", block.Data)
+		fmt.Printf("nonce: %v\n", block.Nonce)
+		fmt.Printf("nonce: %v\n", block.Difficulty)
+
+		pow := blocks.CreateProofOfWork(block)
+		fmt.Printf("Pow validated: %s\n", strconv.FormatBool(pow.ValidateNonce()))
+		fmt.Println()
+	}
 
 }
