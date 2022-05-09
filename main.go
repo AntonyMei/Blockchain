35defe5aec5e6840ac6b62ea33535a669c4e965e
Antony <meiyixuan2000@126.com>
2022-05-09 17:08:30 +0800

    add a new test

--- main.go
+++ main.go
@@ -1,34 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"github.com/AntonyMei/Blockchain/src/blockchain"
-	"github.com/AntonyMei/Blockchain/src/blocks"
-	"strconv"
+	"github.com/AntonyMei/Blockchain/src/transaction"
 )
 
 func main() {
-	println("Persistent Storage Test\n")
-	// create a chain
-	chain := blockchain.InitBlockChain()
-	chain.AddBlock("first block after genesis")
-	chain.AddBlock("second block after genesis")
-	chain.AddBlock("third block after genesis")
+	println("Transaction Test")
+	// alice starts a chain / continues from last chain
+	chain := blockchain.InitBlockChain("Alice")
+	// then mines a block
+	chain.AddBlock("Alice", "First Block after genesis", []*transaction.Transaction{})
+	// bob comes in and mine another block
+	chain.AddBlock("Bob", "Second Block after genesis", []*transaction.Transaction{})
+	// Alice pay bob 30 in the next block
+	tx1 := chain.GenerateTransaction("Alice", "Bob", 30)
+	chain.AddBlock("Bob", "Third Block after genesis", []*transaction.Transaction{tx1})
+	// Alice gives Bob 10, then 40, then Bob returns 60, Charlie logs this
+	tx2 := chain.GenerateTransaction("Alice", "Bob", 10)
+	tx3 := chain.GenerateTransaction("Alice", "Bob", 40)
+	tx4 := chain.GenerateTransaction("Bob", "Alice", 60)
+	chain.AddBlock("Charlie", "Fourth Block after genesis",
+		[]*transaction.Transaction{tx2, tx3, tx4})
 
 	// print info
-	hasNext := true
-	for iterator := chain.Iterator(); hasNext; {
-		block := iterator.GetVal()
-		hasNext = iterator.Next()
-		fmt.Printf("Previous hash: %x\n", block.PrevHash)
-		fmt.Printf("hash: %x\n", block.Hash)
-		fmt.Printf("data: %s\n", block.Data)
-		fmt.Printf("nonce: %v\n", block.Nonce)
-		fmt.Printf("difficulty: %v\n", block.Difficulty)
-
-		pow := blocks.CreateProofOfWork(block)
-		fmt.Printf("Pow validated: %s\n", strconv.FormatBool(pow.ValidateNonce()))
-		fmt.Println()
-	}
+	chain.Log2Terminal()
 
 }
