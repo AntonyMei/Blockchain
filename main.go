d3e66af6ed6a3278a0e63204aeab5e0de40ca7e8
Antony <meiyixuan2000@126.com>
2022-05-09 20:17:33 +0800

    changed log

--- main.go
+++ main.go
@@ -16,11 +16,17 @@ func main() {
 	// Alice pay bob 30 in the next block
 	tx1 := chain.GenerateTransaction("Alice", []string{"Bob"}, []int{30})
 	chain.AddBlock("Bob", "Third Block after genesis", []*transaction.Transaction{tx1})
-	// Alice gives Bob 90, then 40, then Bob returns 60, Charlie logs this
+	// Alice gives Bob 90, David 40, then Bob returns 60, Charlie logs this
 	tx2 := chain.GenerateTransaction("Alice", []string{"Bob", "David"}, []int{90, 40})
 	tx3 := chain.GenerateTransaction("Bob", []string{"Alice"}, []int{60})
 	chain.AddBlock("Charlie", "Fourth Block after genesis",
 		[]*transaction.Transaction{tx2, tx3})
+	// At this point the balance should look like
+	// Alice:   100
+	// Bob:     260
+	// Charlie: 100
+	// David:   40
+	// Total:   500
 
 	// print info
 	chain.Log2Terminal()
