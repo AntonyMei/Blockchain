ca633451da9fad1c6d0f114ec5ceadf7ff4d6c62
Antony <meiyixuan2000@126.com>
2022-05-31 21:09:05 +0800

    finished multiprocessing on POW

---
+++ src/blockchain/utxo.go
@@ -0,0 +1,83 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"github.com/AntonyMei/Blockchain/config"
+	"github.com/AntonyMei/Blockchain/src/utils"
+	"io/ioutil"
+	"os"
+)
+
+type UnspentTXO struct {
+	SourceTxID  []byte
+	TxOutputIdx int
+	Value       int
+}
+
+type UTXOSet struct {
+	// UTXO Set: address -> (SourceTxID, TxOutputIdx, Value)
+	Addr2UTXO   map[string][]UnspentTXO
+	UTXOSetPath string
+}
+
+func InitUTXOSet(userName string) *UTXOSet {
+	utxoSet := UTXOSet{}
+	utxoSet.Addr2UTXO = make(map[string][]UnspentTXO)
+	utxoSet.UTXOSetPath = config.PersistentStoragePath + userName + config.UTXOSetPath
+	return &utxoSet
+}
+
+func (utxoSet *UTXOSet) AddUTXO(addr []byte, txo UnspentTXO) {
+	utxoSet.Addr2UTXO[string(addr)] = append(utxoSet.Addr2UTXO[string(addr)], txo)
+}
+
+func (utxoSet *UTXOSet) DeleteUTXO(addr []byte, txo UnspentTXO) {
+	// Find the UTXO
+	var targetIdx = -1
+	for idx, utxo := range utxoSet.Addr2UTXO[string(addr)] {
+		if bytes.Compare(utxo.SourceTxID, txo.SourceTxID) == 0 && utxo.TxOutputIdx == txo.TxOutputIdx {
+			utils.Assert(utxo.Value == txo.Value, "TXO mismatch")
+			targetIdx = idx
+			break
+		}
+	}
+
+	// Delete that UTXO
+	var length = len(utxoSet.Addr2UTXO[string(addr)])
+	utxoSet.Addr2UTXO[string(addr)][targetIdx] = utxoSet.Addr2UTXO[string(addr)][length-1]
+	utxoSet.Addr2UTXO[string(addr)] = utxoSet.Addr2UTXO[string(addr)][:length-1]
+}
+
+func (utxoSet *UTXOSet) SaveFile() {
+	// encode the wallets
+	var content bytes.Buffer
+	gob.Register(elliptic.P256())
+	encoder := gob.NewEncoder(&content)
+	err := encoder.Encode(utxoSet)
+	utils.Handle(err)
+	// save to file
+	err = ioutil.WriteFile(utxoSet.UTXOSetPath, content.Bytes(), 0644)
+	utils.Handle(err)
+}
+
+func (utxoSet *UTXOSet) LoadFile() error {
+	// check whether wallet file exists
+	if _, err := os.Stat(utxoSet.UTXOSetPath); os.IsNotExist(err) {
+		return err
+	}
+
+	// read the file
+	fileContent, err := ioutil.ReadFile(utxoSet.UTXOSetPath)
+	utils.Handle(err)
+
+	// encode it back into a wallet
+	var tmpUTXOSet UTXOSet
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	err = decoder.Decode(&tmpUTXOSet)
+	utils.Handle(err)
+	utxoSet.Addr2UTXO = tmpUTXOSet.Addr2UTXO
+	return nil
+}
